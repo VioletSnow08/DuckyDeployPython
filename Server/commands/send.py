7253f9c247bdcb710ade30930a48945707e584dd
Violet Lauro <violetsnow08@gmail.com>
2024-01-01 04:23:09 -0500

    Reformatted send.py

--- Server/commands/send.py
+++ Server/commands/send.py
@@ -2,6 +2,8 @@
 from colorama import Fore, Style
 
 requires_id = True
+
+
 def execute(conn, args, clients, id):
     msg = input(Fore.CYAN + "Enter message: " + Style.RESET_ALL)
     clients[id].sendall(msg.encode())
